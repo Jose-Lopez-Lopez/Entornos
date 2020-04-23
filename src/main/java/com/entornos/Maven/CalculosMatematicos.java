06b86d0aee783b45c80a8b9b0322b7aea42f7932
Jose <josed@DESKTOP-88NKDAF>
2020-04-23 13:44:06 +0200

    Version 1.2

--- src/main/java/com/entornos/Maven/CalculosMatematicos.java
+++ src/main/java/com/entornos/Maven/CalculosMatematicos.java
@@ -32,5 +32,16 @@ public class CalculosMatematicos {
 		else
 			return numero * factorial(numero-1);	
 	}
-	
+	public static ArrayList<Integer> listaDePrimosHasta (int numero) {
+		ArrayList<Integer> listaDePrimos = new ArrayList<Integer>();
+		listaDePrimos.add(1);
+		listaDePrimos.add(2);
+		listaDePrimos.add(3);
+		for (int i=5; i<=numero; i+=2) {
+			if(esPrimo(i))
+				listaDePrimos.add(i);
+		}
+		return listaDePrimos;
+		}
+	}
 }
